448646fd59989d0fb64a05de339e7e456ecb1bd5
장지원 <jjwking314@naver.com>
2024-06-28 14:46:14 +0900

    3 taps implemented

--- app/src/main/java/com/example/project1/Frag2.kt
+++ app/src/main/java/com/example/project1/Frag2.kt
@@ -1,28 +1,43 @@
 package com.example.project1
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Frag2.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.Button
 
 class Frag2 : Fragment() {
+    private val PICK_IMAGE_REQUEST = 1
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_frag2, container, false)
 
+        val selectImageButton = view.findViewById<Button>(R.id.select_image_button)
+        selectImageButton.setOnClickListener {
+            openGallery()
+        }
+
         return view
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            // 이미지 선택 후 처리할 코드를 여기에 추가합니다
+            val selectedImageUri = data.data
+            // 선택한 이미지 URI를 사용하여 원하는 작업을 수행합니다
+        }
     }
 }
